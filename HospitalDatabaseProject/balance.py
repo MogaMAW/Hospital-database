8b1c68f83f0927dee0a2190abab02ea578be6ec9
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-05 17:58:34 +0300

    Update balance.py

--- HospitalDatabaseProject/balance.py
+++ HospitalDatabaseProject/balance.py
@@ -3,6 +3,7 @@ def balance(payment,payment_made):
     if payment>payment_made:
         balance=payment-payment_made
         print("We demand you :{:,}".format(balance) )
+        
     elif payment<payment_made:
         balance=payment_made-payment
         print("You have a balance of :{:,}".format(balance) )
