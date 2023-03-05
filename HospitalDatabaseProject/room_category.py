02b477eae8101b9689048fd219e2d95491a930ec
MOGAMAW <92805881+MogaMAW@users.noreply.github.com>
2023-03-05 18:05:06 +0300

    Update room_category.py

--- HospitalDatabaseProject/room_category.py
+++ HospitalDatabaseProject/room_category.py
@@ -1,6 +1,6 @@
 
 def room_category():
-    print("We have these room categories: \n1. Executive rooms\n 2. Classic rooms \n3. Ordinary rooms")
+    print("We have these following room categories: \n1. Executive rooms\n 2. Classic rooms \n3. Ordinary rooms")
 
     choice=int(input("Enter your room category choice from 1 to 3:"))
     while choice>3 or choice<1:
